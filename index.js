3cf47791ca62abed797e081bd1170dc3f7673468
bhushannaikaroskar <bhushannaikaroskar@gmail.com>
2021-07-25 06:05:34 +0530

    refactor: organised code into smaller functions

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ var readlineSync = require('readline-sync');
 
 var score = 0;
 var exit = false;
+var username;
 
 
-var username = readlineSync.question('What is your name? ');
-console.log("\n\nHello " + username + " welcome to my quiz \nHere I will ask questions about myself \nLets see how much you know me\n");
+function greeting(){
+  username = readlineSync.question('What is your name? ');
+  console.log("\n\nHello " + username + " welcome to my quiz \nHere I will ask questions about myself \nLets see how much you know me\n");
+  console.log("Press \'e\' in the answer anytime to exit\n\n");
+}
 
 function play(question, answer) {
 
@@ -30,6 +34,30 @@ function play(question, answer) {
 
   return false;
 
+}
+
+function playGame(){
+  for (var i = 0; i < questions.length; i++) {
+    var isExit = play(questions[i].question, questions[i].answer);
+    if(isExit){
+      break;
+    }
+  }
+  console.log("Your score for the quiz is " + score);
+  console.log("quiz ended\n\n");
+
+}
+
+function displayHighScores(){
+  for(var i=0 ; i<highscores.length ; i++){
+    if(score >= highscores[i].score ){
+      console.log(username+"\t\t" + score);
+      console.log(highscores[i].name + "\t\t" + highscores[i].score);
+      score = 0;
+    }else{
+      console.log(highscores[i].name + "\t\t" + highscores[i].score);
+    }
+  }
 }
 
 
@@ -67,34 +95,17 @@ var questions = [
     question: "Where do I study? ",
     answer: "KJ Somaiya"
   }, {
-    question: "Where do I work? ",
-    answer: "Jobless"
+    question: "Whats my favourite programming language",
+    answer: "Javascript"
   }
 ];
 
 
 while (!exit) {
 
-  console.log("Press \'e\' in the answer anytime to exit\n\n")
-
-  for (var i = 0; i < questions.length; i++) {
-    var isExit = play(questions[i].question, questions[i].answer);
-    if(isExit){
-      break;
-    }
-  }
-  console.log("Your score for the quiz is " + score);
-  console.log("quiz ended\n\n");
-
-  for(var i=0 ; i<highscores.length ; i++){
-    if(score >= highscores[i].score ){
-      console.log(username+"\t\t" + score);
-      console.log(highscores[i].name + "\t\t" + highscores[i].score);
-      score = 0;
-    }else{
-      console.log(highscores[i].name + "\t\t" + highscores[i].score);
-    }
-  }
+  greeting();
+  playGame();
+  displayHighScores();
 
 
   
